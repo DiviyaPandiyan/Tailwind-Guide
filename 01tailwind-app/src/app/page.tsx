2c6d0e33cfedefdd4d7778711167a4766eb77105
Diviya Pandiyan <kdmail9@gmail.com.com>
2024-07-31 09:24:04 +0530

    adding customize

--- 01tailwind-app/src/app/page.tsx
+++ 01tailwind-app/src/app/page.tsx
@@ -2,8 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-purple-800">
+    <>
+    <h1 className="text-red-800 bg-[#bada55] test-sm lg:test-lg">
       Hello world!
     </h1>
+    <h1 className="text-gray-800">
+    Hello world!
+  </h1>
+</>
   );
 }
