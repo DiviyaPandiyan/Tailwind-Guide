e590a254d2680433d7f6e8782d13523b7736b555
Diviya Pandiyan <kdmail9@gmail.com.com>
2024-07-31 23:06:12 +0530

    after batting

--- 01tailwind-app/src/app/page.tsx
+++ 01tailwind-app/src/app/page.tsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1 >
-      Hello world!
-    </h1>
-    <h1 >
-    Hello world!
-  </h1>
-</>
+      <h1 className="text-3xl capitalize underline decoration-red-950 decoration-wavy decoration-3 underline-offset-6">
+        hello world!
+      </h1>
+      <p className='break-all'>Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:uppercase to only apply the uppercase utility on hover
+      Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:uppercase to only apply the uppercase utility on hover
+      </p>
+      <h1 className='truncate bg-orange-950 text-sky-200 font-bold p-6 m-4'>
+      Tailwind lets you conditionally apply utility classes in different states using variant modifiers. For example, use hover:uppercase to only apply the uppercase utility on hover
+      </h1>
+    </>
   );
 }
